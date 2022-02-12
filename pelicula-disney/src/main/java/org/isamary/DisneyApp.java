66ec694511eb8d7b9064674154ced14cfbfb2bdd
mary-puma <pumaisabel029@gmail.com>
2022-02-12 18:53:07 -0300

    se agrego dependencia spring boot

--- pelicula-disney/src/main/java/org/isamary/DisneyApp.java
+++ pelicula-disney/src/main/java/org/isamary/DisneyApp.java
@@ -1,4 +1,12 @@
 package org.isamary;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DisneyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DisneyApp.class, args);
+        System.out.println("ejecutando main");
+    }
 }
